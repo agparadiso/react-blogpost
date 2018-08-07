25815e9a5248848196b56554600bc6a97f7829ab
Gabriel Paradiso <gabriel.paradiso@typeform.com>
2018-08-07 21:07:44 +0200

    [lesson-123] add react router and the first route

---
+++ src/components/posts_index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+    render() {
+        return (
+            <div>
+                PostIndexComponent
+            </div>
+        );
+    }
+}
+
+export default PostsIndex;
