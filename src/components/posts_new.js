00a04a96957583564bb592376571197bde7e9c01
Gabriel Paradiso <gabriel.paradiso@typeform.com>
2018-08-15 00:19:18 +0200

    do the post posts request

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
-
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
     renderField(field) {
@@ -23,7 +24,7 @@ class PostsNew extends Component {
     }
 
     onSubmit(values) {
-        console.log(values);
+        this.props.createPost(values);
     }
     render() {
         const { handleSubmit } = this.props;
@@ -72,4 +73,6 @@ function validate (values) {
 export default reduxForm({
     validate,
     form: 'PostNewForm'
-})(PostsNew);
+})(
+    connect(null, { createPost })(PostsNew)
+);
