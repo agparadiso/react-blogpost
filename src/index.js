1aaf7a22f36f1c42fed38dd90ac136c7fa4fcf37
Gabriel Paradiso <gabriel.paradiso@typeform.com>
2018-08-14 21:11:10 +0200

    fetch posts and form with validation for new posts

--- src/index.js
+++ src/index.js
@@ -2,18 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PostIndex from './components/posts_index';
+import PostsNew from './components/posts_new'
+import promise from 'redux-promise';
 
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostIndex}/>
+        <Switch>
+          <Route path="/posts/new" component={PostsNew}/>
+          <Route path="/" component={PostIndex}/>
+        </Switch>
       </div>  
     </BrowserRouter>
   </Provider>
